b6dbdb03c7145650e4b4c7b04a1403c739d7b30a
flippyyt <119715386+flippyyt@users.noreply.github.com>
2022-12-03 13:07:30 -0600

    Add files via upload

---
+++ js/handleKeyboard.js
@@ -0,0 +1,28 @@
+console.log('keyboard')
+document.addEventListener('keydown', event => {
+    if ([32, 37, 38, 39, 40].includes(event.keyCode)) {
+    	event.preventDefault();
+    }
+    switch (event.keyCode) {
+      case 40:
+        fallingPiece.moveDown();
+        if (!playfield.isValid(fallingPiece))
+        fallingPiece.moveUp()
+        else
+          fallingPiece.resetBuffer()
+        break;
+      case 37:
+        fallingPiece.moveLeft();
+        if (!playfield.isValid(fallingPiece))
+        fallingPiece.moveRight()
+        break;
+      case 39:
+        fallingPiece.moveRight();
+        if (!playfield.isValid(fallingPiece))
+        fallingPiece.moveLeft()
+        break;
+    }
+    });
+        
+
+                        
